03f7b2e08644a2e684e4f0ec7e5b2f8a00a22949
MixerBaikalLover <mixergamechannel@gmail.com>
2022-03-04 16:03:40 +0300

    добавил прикол и один туду

--- lib/pages/objects/floor.dart
+++ lib/pages/objects/floor.dart
@@ -1,19 +1,15 @@
-import 'dart:html';
-
 import 'package:block_house_calculator/pages/objects/wall.dart';
 
 class Floor {
   int index;
-  List<Wall> walls =[]   ;
+  List<Wall> walls =[]   ; // выглядит как смайлик, смешно? я смеялся.
   int doorsCount = 0;
-
 
   String getName(){
     return "Этаж " + (index + 1).toString();
   }
 
   Floor(this.index);
-
   void addWall(){
     walls.add(Wall());
   }
