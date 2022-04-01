736dbdfc2e56c71f4bec23aec9a6b7da8150a9a0
CpedaMonsterEnergyLover <Vova.Cpeda@yandex.ru>
2022-04-01 13:04:21 +0300

    Удалил wall.dart и risovalka.dart

--- lib/pages/edit_floor/floor_page.dart
+++ lib/pages/edit_floor/floor_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 class FloorPage extends StatelessWidget {
   final Floor floor;
 
-  const FloorPage({Key? key, required this.floor}) : super(key: key);
+  FloorPage({Key? key, required this.floor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,37 +21,36 @@ class FloorPage extends StatelessWidget {
             },
           ),
         ),
-        body: ListView( children :[
-          Container(child: const SizedBox(width: 300, height:300,
-              child: Align(alignment : Alignment.center,
-                  child: Text('Здесь будет рисовалка надеюсь мы ее сможем сделать', style: TextStyle(fontSize: 20), textAlign: TextAlign.center)
-              )
-          ),
-              decoration: const BoxDecoration(
-              color: Colors.blueGrey,
+        body: Column(
+          children: [
+            SizedBox(width: 300, height:300,
+                child: Container(
+                color: const Color.fromARGB(1, 213, 213, 213),
+                child: const Text("я гей"),
+                ),
+              ),
+            GeneralButton(
+              text: "Добавить Стену", callback: () {
+
+              },
+              longPressCallback: () {},
             ),
-          ),
-          GeneralButton(
-            text: "Добавить Стену", callback: () {
+            GeneralButton(
+              text: "Добавить Дверь",
+              callback: () {
 
-            },
-            longPressCallback: () {},
-          ),
-          GeneralButton(
-            text: "Добавить Дверь",
-            callback: () {
+              },
+              longPressCallback: () {},
+            ),
+            GeneralButton(
+              text: "Добавить Окно",
+              callback: () {
 
-            },
-            longPressCallback: () {},
-          ),
-          GeneralButton(
-            text: "Добавить Окно",
-            callback: () {
-
-            },
-            longPressCallback: () {},
-          ),]
-    ),
+              },
+              longPressCallback: () {},
+            ),
+          ],
+        ),
     );
 
   }
--- lib/pages/objects/floor.dart
+++ lib/pages/objects/floor.dart
@@ -1,34 +1,25 @@
 import 'dart:convert';
 
-import 'package:block_house_calculator/pages/objects/wall.dart';
-
 class Floor {
   int index;
-  List<Wall> walls =[]   ; // выглядит как смайлик, смешно? я смеялся.
+  List<double> walls =[]   ; // выглядит как смайлик, смешно? я смеялся.
   int doorsCount = 0;
   int windowsCount = 0;
+
 
   String getName(){
     return "Этаж " + (index + 1).toString();
   }
 
   Floor(this.index);
-  Floor.full(this.index, this.walls, this.doorsCount, this.windowsCount);
+  Floor.full(this.index, this.walls,this.doorsCount, this.windowsCount);
 
-
-  void addWall(){
-    walls.add(Wall());
-  }
 
 
 
   Map<String, dynamic> toJson(){
     Map<String, dynamic> json = {};
 
-    Map<String, dynamic> wallsMap = {};
-    for (var wall in walls) {
-      wallsMap[wall.toString()] = wall.toJson();
-    }
 
     json = {
       "index": index,
