c1f7341580b065e6a788154d66ce55d5a8788d40
CpedaMonsterEnergyLover <Vova.Cpeda@yandex.ru>
2022-04-01 14:38:51 +0300

    Добавил класс double_input_field.dart

--- lib/pages/edit_house/house_page.dart
+++ lib/pages/edit_house/house_page.dart
@@ -1,5 +1,6 @@
 import 'package:block_house_calculator/pages/edit_floor/floor_page.dart';
 import 'package:block_house_calculator/pages/edit_roof/roof_page.dart';
+import 'package:block_house_calculator/pages/elements/double_input_field.dart';
 import 'package:block_house_calculator/pages/elements/general_button.dart';
 import 'package:block_house_calculator/pages/elements/dialog.dart';
 import 'package:block_house_calculator/pages/objects/house.dart';
@@ -103,54 +104,48 @@ class _HousePageState extends State<HousePage> {
             },
             longPressCallback: () {},
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Длина дома', hintText: widget.house.houseLength.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Длина дома',
+            hintText: widget.house.houseLength.toString(),
+            callback: (value) {
               setState((){widget.house.houseLength = double.parse(value);});
             },
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Ширина дома', hintText: widget.house.houseWidth.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Ширина дома',
+            hintText: widget.house.houseWidth.toString(),
+            callback: (value) {
               setState((){widget.house.houseWidth = double.parse(value);});
             },
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Высота этажей', hintText: widget.house.floorHeight.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Высота этажей',
+            hintText: widget.house.floorHeight.toString(),
+            callback: (value) {
               setState((){widget.house.floorHeight = double.parse(value);});
             },
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Высота дверных проемов', hintText: widget.house.doorHeight.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Высота дверных проемов',
+            hintText: widget.house.doorHeight.toString(),
+            callback: (value) {
               setState((){widget.house.doorHeight = double.parse(value);});
             },
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Высота оконных проемов', hintText: widget.house.windowHeight.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Высота оконных проемов',
+            hintText: widget.house.windowHeight.toString(),
+            callback: (value) {
               setState((){widget.house.windowHeight = double.parse(value);});
             },
           ),
-          TextField(
-            autofocus: false,
-            keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
-            decoration: InputDecoration(labelText: 'Высота оплётки', hintText: widget.house.opletkaHeight.toString()),
-            onSubmitted: (value) {
+          DoubleInputField(
+            labelText: 'Высота оплётки',
+            hintText: widget.house.opletkaHeight.toString(),
+            callback: (value) {
               setState((){widget.house.opletkaHeight = double.parse(value);});
             },
-          ),
+          )
         ]
       ),
     );
---
+++ lib/pages/elements/double_input_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DoubleInputField extends StatelessWidget {
+
+  final String labelText;
+  final String hintText;
+  final Function callback;
+
+  const DoubleInputField({Key? key, required this.labelText, required this.callback, required this.hintText})
+      : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return           TextField(
+      autofocus: false,
+      keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: true),
+      decoration: InputDecoration(labelText: labelText, hintText: hintText),
+      onSubmitted: (value) {
+        callback(value);
+      },
+    );
+  }
+
+
+
+}
