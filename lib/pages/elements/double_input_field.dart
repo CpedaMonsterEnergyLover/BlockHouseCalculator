8dbe51e4e89bacc1945f6c69ba472ee7e4cae720
CpedaMonsterEnergyLover <Vova.Cpeda@yandex.ru>
2022-04-07 16:30:27 +0300

    починил интеджеринпутфиелд

--- lib/pages/elements/double_input_field.dart
+++ lib/pages/elements/double_input_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class IntegerInputField extends StatelessWidget {
+class IntegerInputField extends StatefulWidget {
 
   final String labelText;
   final String hintText;
@@ -10,19 +10,36 @@ class IntegerInputField extends StatelessWidget {
   const IntegerInputField({Key? key, required this.labelText, required this.callback, required this.hintText})
       : super(key: key);
 
+  @override
+  State<IntegerInputField> createState() => _IntegerInputFieldState();
+}
+
+class _IntegerInputFieldState extends State<IntegerInputField> {
+  String errorText = '';
 
   @override
   Widget build(BuildContext context) {
     return           TextField(
       autofocus: false,
       keyboardType:  const TextInputType.numberWithOptions(signed: true, decimal: false),
-      decoration: InputDecoration(labelText: labelText + ": " + hintText + "мм", hintText: hintText),
-      onSubmitted: (value) {
-        callback(value);
+      decoration: InputDecoration(
+          labelText: widget.labelText + ": " + widget.hintText + "мм",
+          hintText: widget.hintText,
+          errorText: errorText == '' ? null : errorText),
+      onChanged: (value) {
+        int? intValue = int.tryParse(value);
+        if (intValue != null) {
+          widget.callback(intValue);
+          setState(() {
+            errorText = "";
+          });}
+        else
+        {
+          setState(() {
+            errorText = "Неправильное значение";
+          });
+        }
       },
     );
   }
-
-
-
 }
